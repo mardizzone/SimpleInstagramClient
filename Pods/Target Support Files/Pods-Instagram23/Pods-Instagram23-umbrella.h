fe8e79704b65827820fef9c55f2f060f0ef5694b
Michael Ardizzone <mjardizzone@gmail.com>
2018-01-15 12:05:41 -0800

    add Cocoapods

---
+++ Pods/Target Support Files/Pods-Instagram23/Pods-Instagram23-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Instagram23VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Instagram23VersionString[];
+
